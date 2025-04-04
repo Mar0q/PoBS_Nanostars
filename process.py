fc927322ec91179a623e80f56ac2520bbe86c7da
Maria <elsamariaes@gmail.com>
2025-04-04 11:34:12 +0200

    process file = getting data plotted

---
+++ process.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+results = pd.read_csv("figures\simulation_results.csv", delimiter = ";")
+
+
+results["Coverage"] = (17000-results["P"]) / 17000
+
+print(results)
+
+
+plt.figure(figsize=(3, 6))
+plt.plot(results["R"], results["Coverage"], marker='o', linestyle='-', label="k_ratio = 10^6")
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel("R")
+plt.ylabel("Coverage")
+plt.title("Coverage vs Free Receptors")
+plt.legend()
+plt.show()
